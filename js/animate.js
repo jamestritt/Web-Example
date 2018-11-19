df7ed265d0d5dbbed4b7354d32ef38e49fd8eb78
Jim <40718698+jamestritt@users.noreply.github.com>
2018-11-19 12:48:39 -0500

    Add files via upload

---
+++ js/animate.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    var animation = document.getElementById('content')
+        content.style.marginLeft = "250px";
+           
+    var bottomAnimation = document.getElementById('button')
+        button.style.marginLeft = "250px";
+};
+           
---
+++ js/test.js
@@ -0,0 +1,7 @@
+function date() {
+  document.getElementById("date").innerHTML = new Date().toString()
+}
+
+function myList() {
+    document.getElementById("facts").innerHTML = "<li>Increase Flexibility</li> <br> <li>Yoga is over 5,000 years old</li> <br> <li>There are 19 different types of Yoga</li> <br> <li>Improves posture</li> <br> <li>US spend $5.7 billion a year on yoga</li>";
+}
